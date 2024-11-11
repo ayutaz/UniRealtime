49076159f632743d709bba1cd7ada88f0695c166
ayutaz <ka1357amnbpdr@gmail.com>
2024-11-12 00:25:47 +0900

    fix: Androidおよび他のプラットフォームでのマイクの取得処理

--- Assets/UniRealtime/Scripts/AudioRecorder.cs
+++ Assets/UniRealtime/Scripts/AudioRecorder.cs
@@ -10,40 +10,53 @@ namespace UniRealtime
         /// <summary>
         /// Use Microphone name
         /// </summary>
-        public string Microphone { get;private set; }
+        public string Microphone { get; private set; }
 
         /// <summary>
         ///　AudioClip to store the audio data obtained from the microphone
         /// </summary>
         public AudioClip AudioClip { get; private set; }
-        
+
         /// <summary>
         /// The sample rate of the AudioClip produced by the recording
         /// </summary>
         private const int SampleRate = 24000;
-        
+
         /// <summary>
         /// Is the length of the AudioClip produced by the recording.
         /// </summary>
         private const int ClipLength = 1;
-        
+
         /// <summary>
         /// Constructor
         /// </summary>
-        public AudioRecorder()
+        public AudioRecorder(string microphoneDeviceName = "")
         {
-            // マイクの初期化
-            if (UnityEngine.Microphone.devices.Length > 0)
+            if (string.IsNullOrEmpty(microphoneDeviceName))
             {
-                Microphone = UnityEngine.Microphone.devices[0];
+                // マイクデバイス名が指定されていない場合、プラットフォームごとに設定
+#if UNITY_ANDROID
+                microphoneDeviceName = "Android audio input";
+#else
+                if (UnityEngine.Microphone.devices.Length > 0)
+                {
+                    Microphone = UnityEngine.Microphone.devices[0];
+                }
+                else
+                {
+                    Debug.LogError("マイクが接続されていません");
+                }
+#endif
                 AudioClip = UnityEngine.Microphone.Start(Microphone, true, ClipLength, SampleRate);
-            } else
+            }
+            else
             {
-                Debug.LogError("マイクが接続されていません");
+                Microphone = microphoneDeviceName;
+                AudioClip = UnityEngine.Microphone.Start(Microphone, true, ClipLength, SampleRate);
             }
         }
-        
-        
+
+
         /// <summary>
         /// Start recording
         /// </summary>
@@ -51,7 +64,7 @@ namespace UniRealtime
         {
             UnityEngine.Microphone.Start(Microphone, true, ClipLength, SampleRate);
         }
-        
+
         /// <summary>
         /// Stop recording
         /// </summary>
